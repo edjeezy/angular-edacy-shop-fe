617b8cca2fdefe3897d305500ef51907c252d2b4
Mouhamed Edouard Junior Mar <mouhamededji@gmail.com>
2024-03-26 20:18:48 +0000

    ⚠️ import zone,js (remove before prod)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import 'zone.js';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
