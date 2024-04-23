b55be11b2a6da0b23d8740c095164795069e1b23
Mouhamed Edouard Junior Mar <mouhamededji@gmail.com>
2024-04-23 18:16:12 +0000

    intro au formControl

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../../shared/services/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { delay, tap } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -8,16 +10,54 @@ import { AuthService } from '../../shared/services/auth.service';
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
-  email!: string;
+
+  email: FormControl = new FormControl('', [Validators.email, Validators.required]);
   password!: string;
+
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.minLength(6), Validators.required]),
+  })
+
   constructor(
     private authService: AuthService,
   ) {}
 
   ngOnInit(): void {
+    this.email.valueChanges.pipe(
+      // Ajoute un delai de x millisecondes
+      delay(2300),
+      tap(console.log)
+    ).subscribe();
+  }
+
+  setEmail() {
+      /* METTRE UNE VALEUR DEPUIS LE TEMPLATE */
+      this.email.setValue('test3@test4.com');
+  }
+  
+  handleActivation() {
+    /* ENLEVER LES VALIDATEURS */
+    this.email.clearValidators();
+    this.email.updateValueAndValidity();
+
+    /* GERER LE DISABLED STATE */
+    this.email.disabled ? this.email.enable() : this.email.disable();
+  }
+
+  testEmail() {
+    console.log('----------------------------------------');
+    console.log('valeur', this.email.value);
+    console.log('errors', this.email.errors);
+    console.log('dirty (il a ete touche) ', this.email.dirty);
+    console.log('pristine (il a pas encore touche)', this.email.pristine);
+    console.log('disabled (desactive)', this.email.disabled);
+    console.log('enabled (active)', this.email.enabled);
+    console.log('valid', this.email.valid);
+    console.log('invalid', this.email.invalid);
   }
   
   onSubmit() {
-    this.authService.login(this.email, this.password);
+    //this.authService.login('', this.email, ;this.password);
   }
 }
